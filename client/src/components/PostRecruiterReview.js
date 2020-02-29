2113bf49b8b5914ebc48539a57332b4d98042152
electrone901 <carbajalluis@rocketmail.com>
2020-02-29 14:57:59 -0500

    added recruiter view

--- client/src/components/PostRecruiterReview.js
+++ client/src/components/PostRecruiterReview.js
@@ -1,16 +1,52 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 // make your own css on Styles Folder file and import here. Remove the this comment
 // import './App.css';
 
-class PostRecruiterReview extends Component{
-  render(){
-      
-      return(
-          <div>
-              <h1>Post Recruiter Reviewm Form</h1>
+class PostRecruiterReview extends Component {
+  constructor() {
+    super();
+    this.state = {
+      author: "",
+      decription_review: ""
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChange(e) {
+    console.log("click", this.state);
+    this.setState({ decription_review: e.target.value });
+  }
+
+  onSubmit(e) {
+    console.log("click", this.state);
+    // this.props.postReview(reviewData, this.props.match.params.id, this.props.history);
+  }
+  render() {
+    return (
+      <div className="card-content valign center">
+        <h1>Add a Review</h1>
+        <form onSubmit={this.onSubmit}>
+          <div className="row">
+            <div className="input-field col s6">
+              <label htmlFor="text">Your review</label>
+              <textarea
+                type="text"
+                id="decription_review"
+                min="5"
+                className="form-control form-control-lg"
+                name="decription_review"
+                value={this.state.decription_review}
+                onChange={this.onChange}
+                rows="3"
+              ></textarea>
+            </div>
           </div>
-      );
+          <input type="submit" className="waves-effect waves-light btn"></input>
+        </form>
+      </div>
+    );
   }
 }
-export default (PostRecruiterReview);
+export default PostRecruiterReview;
--- client/src/components/RateARecruiter.js
+++ client/src/components/RateARecruiter.js
@@ -1,15 +1,49 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import "../styles/recruiter.css";
 
-// make your own css on Styles Folder file and import here. Remove the this comment
-// import './App.css';
-class RateARecruiter extends Component{
-  render(){
-      
-      return(
-          <div>
-              <h1>Rate A Recruiter</h1>
+class RateARecruiter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      recruiterName: ""
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ recruiterName: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    // redirect to profile
+  }
+
+  render() {
+    return (
+      <div className="card-content valign center">
+        <h1 className="">Rate your recruiter</h1>
+        <form onSubmit={this.onSubmit}>
+          <div className="row">
+            <div className="input-field col offset-s4 s4">
+              <input
+                name="recruiterName"
+                value={this.state.recruiterName}
+                onChange={this.onChange}
+                id="recruiterName"
+                type="text"
+              />
+              <label className="active" for="recruiterName">
+                Recruiter Name
+              </label>
+            </div>
           </div>
-      );
+
+          <input type="submit" className="waves-effect waves-light btn"></input>
+        </form>
+      </div>
+    );
   }
 }
-export default (RateARecruiter);
+export default RateARecruiter;
