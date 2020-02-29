ae76b7ff4de4f0eb3b4f1c76f9a3764bdac1693a
Zafir Hasan <zafirhsn@gmail.com>
2020-02-29 14:24:40 -0500

    Added search buttons

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,73 @@
 import React, { Component } from "react";
 import '../styles/Home.css'
 
-class Home extends Component {
-  render(){
-      return(
-        <div className="home"> 
-          Hello from home
-        </div>
 
-      );
+class Home extends Component {
+  state = {
+    clickedSearchbyRecruiter: false,
+    clickedSearchbyCompany: false,
+    rateaRecruiter: false
+  }
+  searchByrecruiterEvent(e) {
+    this.state.clickedSearchbyRecruiter = !this.state.clickedSearchbyRecruiter;
+    console.log(this.state.clickedSearchbyRecruiter);
+  }
+  searchByCompanyEvent(e) {
+    this.state.clickedSearchbyCompany = !this.state.clickedSearchbyCompany;
+    console.log(this.state.clickedSearchbyCompany);
+  }
+  rateaRecruiterEvent(e) {
+    this.state.rateaRecruiter = !this.state.rateaRecruiter;
+    console.log(this.state.rateaRecruiter);
+  }
+  render(){
+    return(
+      <div className="home"> 
+        <div className="container">
+
+          {/* Search bar row */}
+          <div className="row">
+              <form className="col">
+                <div className="row">
+                  <div className="input-field col">
+                    <input placeholder="Search" name="searchbar" type="text" id="search_bar"></input>
+            
+                  </div>
+                </div>
+              </form>            
+          </div>
+
+          {/* Button component row */}
+          <div className="row">
+            <div className="col">
+            <a className="waves-effect waves-light btn-large search-button" onClick={this.searchByrecruiterEvent.bind(this)}>Search by Recruiter</a>
+            </div>
+            <div className="col">
+            <a className="waves-effect waves-light btn-large search-button" onClick={this.searchByCompanyEvent.bind(this)}>Search by Company</a>
+            </div>
+            <div className="col">
+            <a className="waves-effect waves-light btn-large search-button" onClick={this.rateaRecruiterEvent.bind(this)}>Rate a Recruiter</a>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="row">
+              <div className="col">
+                Joe Smith
+              </div>
+              <div className="col">
+                recruiter
+              </div>
+              <div className="col">
+                3/5
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    );
   }
 }
 
